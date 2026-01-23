05cfe8bf505aa99b2944c0d27e914b850ea3e230
Marck <myohalmoam@gmail.com>
2026-01-23 13:35:38 -0600

    solved exercise 06 of js

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    // return  str.repeat(num)
+    repeat = ""
+    if (num < 0 ) {
+        return repeat = "ERROR"
+    }
+    for (let i = 0; i < num; i++ )
+        repeat += str
 
+    return repeat
 };
 
 // Do not edit below this line
