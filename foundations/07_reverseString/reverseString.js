36459be33b29ab2d9c3d3867d24d5c6feb65def3
Marck <myohalmoam@gmail.com>
2026-01-23 14:45:19 -0600

    solved exercise 07 of js

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let rev_str = ""
+    let len = str.length
 
+    for (let i = len - 1 ; i >= 0; i--) {
+        let char = str[i]
+        rev_str += char
+    }   
+    return  rev_str
 };
 
 // Do not edit below this line
