5b085436fedcd450e7ec6fdfb40ce243f24240eb
Marck <myohalmoam@gmail.com>
2026-02-02 13:39:06 -0600

    finished first test of ex 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, valueToRemove) {
+    itemIdex = arr.indexOf(valueToRemove);
+    arr.splice(itemIdex, 1)
+    return arr;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
