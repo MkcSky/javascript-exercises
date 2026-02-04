096d31cfe8233188a8f093918ada58c8622d70cd
Marck <myohalmoam@gmail.com>
2026-02-04 13:08:22 -0600

    finished ex 09 of js

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,40 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let arr = [];
+    let min;
+    let max;
+    
+    if (num1 < 0 || num2 < 0){
+        return "ERROR"
+    }
+    else if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR"
+    }
+    else if (num1 < num2){
+        min = num1
+        max = num2
+    }
+    else if (num1 > num2) {
+        min = num2
+        max = num1
+    }
+    else {
+        return "ERROR"
+    }
 
+    let count = min
+    let addedNums = min
+
+    while(count <= max){
+        arr.push(addedNums)
+        addedNums++;
+        count++;
+    }
+
+    let sumArr = arr.reduce((acc, num) => {
+        return acc + num;
+    })
+
+    return sumArr;
 };
 
 // Do not edit below this line
